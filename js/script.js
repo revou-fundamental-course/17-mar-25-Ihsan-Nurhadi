817d0d8484d04066ffefc08b33ff93e56184efa6
Naschi <naschivnr28@gmail.com>
2025-03-23 13:23:11 +0700

    Perbaikan algoritma

--- js/script.js
+++ js/script.js
@@ -11,12 +11,19 @@ function toggleCircumferenceButton() {
 function calculateArea() {
     const side = document.getElementById('side').value;
     const area = side * side;
-    document.getElementById('result').innerText = `Luas Persegi: ${area} satuan²`;
+    document.getElementById('result').innerText = 
+    `Luas = S X S\n` +
+    `Luas = ${side} X ${side}\n` +
+    `Luas = ${area} satuan²`;
 }
+
 function calculateCircumference() {
     const side = document.getElementById('sideCircumference').value;
     const circumference = side * 4; // Menghitung keliling
-    document.getElementById('resultCircumference').innerText = `Keliling Persegi: ${circumference} satuan`;
+    document.getElementById('resultCircumference').innerText = 
+    `Keliling = 4 X S\n` +
+    `Keliling = 4 X ${side}\n` +
+    `Keliling = ${circumference} satuan`;
 }
 function triggerbutton() {
     const panjang = document.getElementById('panjang').value;
@@ -36,11 +43,17 @@ function menghitungluas() {
     const panjang = document.getElementById('panjang').value;
     const lebar = document.getElementById('lebar').value;
     const luas = panjang * lebar; // Menghitung luas persegi panjang
-    document.getElementById('hasilluas').innerText = `Luas Persegi Panjang: ${luas} satuan²`;
+    document.getElementById('hasilluas').innerText = 
+    `Luas = P X L\n` +
+    `Luas = ${panjang} X ${lebar}\n` +
+    `Luas = ${luas} satuan²`;
 }
 function menghitungkeliling() {
     const panjang = document.getElementById('panjang2').value;
     const lebar = document.getElementById('lebar2').value;
     const keliling = 2 * panjang + 2 * lebar;
-    document.getElementById('hasilkeliling').innerText = `Keliling Persegi Panjang: ${keliling} satuan`;
+    document.getElementById('hasilkeliling').innerText = 
+    `Keliling = 2 X (P + l)\n` +
+    `Keliling = 2 X (${panjang}+${lebar})\n` +
+    `Keliling = ${keliling} satuan`;
 }
